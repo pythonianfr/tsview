c4c0b35447a540b3ed2f054205e3cc8c4463cad1
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2019-03-04 11:10:42 +0100

    follow tshistory schema changes

--- conftest.py
+++ conftest.py
@@ -1,14 +1,25 @@
 from pathlib import Path
 
 import pytest
-from pytest_sa_pg import fixture
+from pytest_sa_pg import db
+from sqlalchemy import create_engine
 from tshistory import schema
 
 DATADIR = Path(__file__).parent / 'test/data'
+DBURI = 'postgresql://localhost:5433/postgres'
 
-engine = fixture.engine_fixture(schema.meta, DATADIR, 5433,
-                                # tshistory schema builder
-                                schema.init)
+
+@pytest.fixture(scope='session')
+def engine(request):
+    db.setup_local_pg_cluster(request, DATADIR, 5433, {
+        'timezone': 'UTC',
+        'log_timezone': 'UTC'}
+    )
+    e = create_engine(DBURI)
+    sch = schema.tsschema()
+    sch.destroy(e)
+    schema.init_schemas(e)
+    return e
 
 
 def pytest_addoption(parser):
