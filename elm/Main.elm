2661b15e2531fe66c097b3707d74f76ba6e0d87b
André Espaze <dede@pythonian.fr>
2019-07-03 11:50:05 +0200

    Rendering with Tachyons

--- elm/Main.elm
+++ elm/Main.elm
@@ -3,8 +3,10 @@ module Main exposing (main)
 import Browser
 import Dict
 import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (class)
 import Http
 import Json.Decode as Decode
+import Tachyons.Classes as T
 
 
 type alias Model =
@@ -19,6 +21,11 @@ type alias SeriesCatalog =
 
 type Msg
     = CatalogReceived (Result Http.Error SeriesCatalog)
+
+
+classes : List String -> Attribute msg
+classes xs =
+    class (String.join " " xs)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -37,7 +44,23 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    ul [] <| List.map (\x -> li [] [ text x ]) model.series
+    let
+        ul_class =
+            classes [ T.list, T.pl0, T.ml0, T.center, T.mw5, T.ba, T.b__light_silver, T.br3 ]
+
+        li_class =
+            classes [ T.ph3, T.pv2, T.bb, T.b__light_silver ]
+
+        renderSeries xs =
+            ul [ ul_class ] <| List.map (\x -> li [ li_class ] [ text x ]) xs
+
+        article_class =
+            classes [ T.mw5, T.mw6_ns, T.center, T.pt4 ]
+
+        div_class =
+            classes [ T.aspect_ratio, T.aspect_ratio__1x1, T.mb4 ]
+    in
+    article [ article_class ] [ div [ div_class ] [ renderSeries model.series ] ]
 
 
 main : Program () Model Msg
