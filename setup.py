fa0778edce315827cd7fa4cc8fe3b73d4ea2128f
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2019-09-18 09:54:18 +0200

    pkg: prepare 0.8

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class ElmBuild(build_ext):
 
 
 setup(name='tsview',
-      version='0.7.0',
+      version='0.8.0',
       author='Pythonian',
       author_email='aurelien.campeas@pythonian.fr',
       description=('Plugin to `tshistory` which provides a `view` subcommand '
