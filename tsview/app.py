21f8ab397b319eac79cfc12aa6e1f72e31352fbc
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2019-09-26 16:37:57 +0200

    adjust for the new tshistory_rest api handling

--- tsview/app.py
+++ tsview/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from sqlalchemy import create_engine
+from tshistory.api import timeseries
 from tshistory_rest.blueprint import blueprint as rest_blueprint
 
 from tsview.blueprint import tsview
@@ -13,7 +14,7 @@ app = Flask('tsview')
 def kickoff(host, port, dburi, debug=False):
     engine = create_engine(dburi)
     app.register_blueprint(
-        rest_blueprint(engine),
+        rest_blueprint(engine.url),
         url_prefix='/api'
     )
     app.register_blueprint(tsview(engine))
