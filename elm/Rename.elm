78cd17492fe310deb52f303b8ebc06767231a76a
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2020-01-07 15:55:29 +0100

    elm/rename: small style adjustments

--- elm/Rename.elm
+++ elm/Rename.elm
@@ -1,18 +1,3 @@
-{- Compilation :
-
-    $ sed -e "s/%APP%/Rename/" \
-    -e "s/%URLPREFIX%/http:\/\/tshistory.test.pythonian.fr/" \
-    elm/index.html.in > tsrename/index.html
-    $ elm make --output tsrename/elm.js elm/Rename.elm
-
-   Running :
-
-    $ cd elm
-    $ elm reactor&
-
--}
-
-
 module Rename exposing (main)
 
 import Browser
@@ -67,7 +52,7 @@ getCatalog : String -> Cmd Msg
 getCatalog urlPrefix =
     Http.get
         { expect =
-            Common.expectJsonMessage CatalogReceived (Decode.dict Decode.string)
+              Common.expectJsonMessage CatalogReceived (Decode.dict Decode.string)
         , url =
             UB.crossOrigin urlPrefix
                 [ "api", "series", "catalog" ]
@@ -86,123 +71,119 @@ update msg model =
                 Just current ->
                     if current == new then
                         Nothing
-
                     else
                         Just new
 
                 Nothing ->
                     Just new
 
-        newModel x =
-            ( x, Cmd.none )
+        newModel x = ( x, Cmd.none )
 
         keywordMatch xm xs =
             if String.length xm < 2 then
                 []
-
             else
                 KeywordSelector.select xm xs |> List.take 20
     in
-    case msg of
-        CatalogReceived (Ok x) ->
-            let
-                series =
-                    Dict.keys x
-            in
-            ( { model
-                | series = series
-                , searchedSeries = keywordMatch model.searchString model.series
-              }
-            , Cmd.none
-            )
+        case msg of
+            CatalogReceived (Ok x) ->
+                let
+                    series = Dict.keys x
+                in
+                    ( { model
+                          | series = series
+                          , searchedSeries = keywordMatch model.searchString model.series
+                      }
+                    , Cmd.none
+                    )
 
-        CatalogReceived (Err x) ->
-            newModel { model | error = Just x }
+            CatalogReceived (Err x) ->
+                newModel { model | error = Just x }
 
-        ToggleItem x ->
-            newModel { model | selectedSerie = toggleItem x model.selectedSerie }
+            ToggleItem x ->
+                newModel { model | selectedSerie = toggleItem x model.selectedSerie }
 
-        SearchSeries x ->
-            newModel { model | searchString = x }
+            SearchSeries x ->
+                newModel { model | searchString = x }
 
-        MakeSearch ->
-            newModel
+            MakeSearch ->
+                newModel
                 { model
                     | searchedSeries = keywordMatch model.searchString model.series
                 }
 
-        EditMode ->
-            newModel
+            EditMode ->
+                newModel
                 { model
                     | state = Edit
                     , renamedSerie = Maybe.withDefault "" model.selectedSerie
                 }
 
-        SelectMode ->
-            newModel { model | state = Select, error = Nothing }
+            SelectMode ->
+                newModel { model | state = Select, error = Nothing }
 
-        NewSerieName x ->
-            newModel { model | renamedSerie = x, error = Nothing }
+            NewSerieName x ->
+                newModel { model | renamedSerie = x, error = Nothing }
 
-        OnRename ->
-            let
-                expect =
-                    Common.expectJsonMessage RenameDone Decode.string
+            OnRename ->
+                let
+                    expect =
+                        Common.expectJsonMessage RenameDone Decode.string
 
-                url =
-                    UB.crossOrigin model.urlPrefix
-                        [ "api", "series", "state" ]
-                        [ UB.string "name" <|
-                            Maybe.withDefault "" model.selectedSerie
-                        , UB.string "newname" model.renamedSerie
-                        ]
+                    url =
+                        UB.crossOrigin model.urlPrefix
+                            [ "api", "series", "state" ]
+                            [ UB.string "name" <|
+                                  Maybe.withDefault "" model.selectedSerie
+                            , UB.string "newname" model.renamedSerie
+                            ]
 
-                putRequest =
-                    Http.request
-                        { method = "PUT"
-                        , headers = []
-                        , url = url
-                        , body = Http.emptyBody
-                        , expect = expect
-                        , timeout = Nothing
-                        , tracker = Nothing
-                        }
-            in
-            ( model, putRequest )
+                    putRequest =
+                        Http.request
+                            { method = "PUT"
+                            , headers = []
+                            , url = url
+                            , body = Http.emptyBody
+                            , expect = expect
+                            , timeout = Nothing
+                            , tracker = Nothing
+                            }
+                in
+                    ( model, putRequest )
 
-        RenameDone (Ok _) ->
-            ( { model
-                | state = Select
-                , selectedSerie = Nothing
-                , renamedSerie = ""
-                , error = Nothing
-              }
-            , getCatalog model.urlPrefix
-            )
+            RenameDone (Ok _) ->
+                ( { model
+                      | state = Select
+                      , selectedSerie = Nothing
+                      , renamedSerie = ""
+                      , error = Nothing
+                  }
+                , getCatalog model.urlPrefix
+                )
 
-        RenameDone (Err x) ->
-            newModel { model | error = Just x }
+            RenameDone (Err x) ->
+                newModel { model | error = Just x }
 
 
 selectorConfig : KeywordSingleSelector.Config Msg
 selectorConfig =
     { searchSelector =
-        { action = Nothing
-        , defaultText =
-            text
-                "Type some keywords in input bar for selecting time series"
-        , toggleMsg = ToggleItem
-        }
+          { action = Nothing
+          , defaultText =
+              text
+              "Type some keywords in input bar for selecting time series"
+          , toggleMsg = ToggleItem
+          }
     , actionSelector =
-        { action =
-            Just
+          { action =
+                Just
                 { attrs = [ classes [ T.white, T.bg_blue ] ]
                 , html = text "Rename"
                 , clickMsg = EditMode
                 }
-        , defaultText = text ""
-        , toggleMsg = ToggleItem
-        }
+          , defaultText = text ""
+          , toggleMsg = ToggleItem
+          }
     , onInputMsg = SearchSeries
     , divAttrs = [ classes [ T.aspect_ratio, T.aspect_ratio__1x1, T.mb4 ] ]
     }
@@ -214,51 +195,49 @@ editor model =
         edit =
             let
                 txt =
-                    "New name for : "
-                        ++ Maybe.withDefault "" model.selectedSerie
+                    "New name for : " ++ Maybe.withDefault "" model.selectedSerie
 
                 lab =
                     label
-                        [ classes [ T.db, T.fw1, T.lh_copy ] ]
-                        [ text txt ]
+                    [ classes [ T.db, T.fw1, T.lh_copy ] ]
+                    [ text txt ]
 
                 inputClass =
                     classes
-                        [ T.input_reset
-                        , T.dtc
-                        , T.ba
-                        , T.b__black_20
-                        , T.pa2
-                        , T.db
-                        , T.w_100
-                        ]
+                    [ T.input_reset
+                    , T.dtc
+                    , T.ba
+                    , T.b__black_20
+                    , T.pa2
+                    , T.db
+                    , T.w_100
+                    ]
 
                 inpt =
                     input
-                        [ inputClass
-                        , value model.renamedSerie
-                        , onInput NewSerieName
-                        ]
-                        []
+                    [ inputClass
+                    , value model.renamedSerie
+                    , onInput NewSerieName
+                    ] []
             in
-            div [ classes [ T.mb3 ] ] [ lab, inpt ]
+                div [ classes [ T.mb3 ] ] [ lab, inpt ]
 
         button ( txt, bg_color, toMsg ) =
             let
                 aClass =
                     classes
-                        [ T.link
-                        , T.dim
-                        , T.ph3
-                        , T.pv2
-                        , T.ma2
-                        , T.dib
-                        , T.tc
-                        , T.white
-                        , bg_color
-                        ]
+                    [ T.link
+                    , T.dim
+                    , T.ph3
+                    , T.pv2
+                    , T.ma2
+                    , T.dib
+                    , T.tc
+                    , T.white
+                    , bg_color
+                    ]
             in
-            a [ aClass, onMouseDown toMsg ] [ text txt ]
+                a [ aClass, onMouseDown toMsg ] [ text txt ]
 
         buttonAttrs =
             [ ( "Rename", T.bg_blue, OnRename )
@@ -272,20 +251,20 @@ editor model =
             let
                 cls =
                     classes
-                        [ T.flex
-                        , T.items_center
-                        , T.justify_center
-                        , T.pa4
-                        , T.bg_washed_red
-                        , T.navy
-                        ]
+                    [ T.flex
+                    , T.items_center
+                    , T.justify_center
+                    , T.pa4
+                    , T.bg_washed_red
+                    , T.navy
+                    ]
             in
-            [ div [ cls ] [ text mess ] ]
+                [ div [ cls ] [ text mess ] ]
 
         checkErr xs =
             Common.maybe xs (addErr >> List.append xs) model.error
     in
-    div selectorConfig.divAttrs <| checkErr [ edit, buttons ]
+        div selectorConfig.divAttrs <| checkErr [ edit, buttons ]
 
 
 view : Model -> Html Msg
@@ -306,7 +285,7 @@ view model =
                 Edit ->
                     editor model
     in
-    article [ classes [ T.center, T.pt4, T.w_90 ] ] [ content ]
+        article [ classes [ T.center, T.pt4, T.w_90 ] ] [ content ]
 
 
 main : Program String Model Msg
@@ -314,21 +293,21 @@ main =
     let
         init urlPrefix =
             let
-                p =
-                    Common.checkUrlPrefix urlPrefix
+                p = Common.checkUrlPrefix urlPrefix
             in
-            ( Model p Select [] "" [] Nothing "" Nothing, getCatalog p )
+                ( Model p Select [] "" [] Nothing "" Nothing, getCatalog p )
 
         sub model =
-            if model.state == Select then
+            if
+                model.state == Select
+            then
                 Time.every 1000 (always MakeSearch)
-
             else
                 Sub.none
     in
-    Browser.element
-        { init = init
-        , view = view >> toUnstyled
-        , update = update
-        , subscriptions = sub
-        }
+        Browser.element
+            { init = init
+            , view = view >> toUnstyled
+            , update = update
+            , subscriptions = sub
+            }
