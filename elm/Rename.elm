5a175d5851343a7d2b60ac7e6a33a7185d109af1
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2020-01-13 14:42:08 +0100

    elm/rename: retire the keyword single selector
    It was redundant as we can easily express the "one selected" item constraint
    within rename itself.

--- elm/Rename.elm
+++ elm/Rename.elm
@@ -10,7 +10,7 @@ import Http
 import Catalog exposing (RawSeriesCatalog, SeriesCatalog, buildCatalog, getCatalog)
 import Json.Decode as Decode
 import KeywordSelector
-import KeywordSingleSelector
+import KeywordMultiSelector
 import SeriesSelector
 import Tachyons.Classes as T
 import Time
@@ -150,7 +150,7 @@ update msg model =
                 newModel { model | error = Just x }
 
 
-selectorConfig : KeywordSingleSelector.Config Msg
+selectorConfig : KeywordMultiSelector.Config Msg
 selectorConfig =
     { searchSelector =
           { action = Nothing
@@ -256,16 +256,16 @@ view : Model -> Html Msg
 view model =
     let
         ctx =
-            KeywordSingleSelector.Context
+            KeywordMultiSelector.Context
                 model.search.search
                 model.search.found
-                (List.head model.search.selected)
+                model.search.selected
                 (Maybe.map text model.error)
 
         content =
             case model.state of
                 Select ->
-                    KeywordSingleSelector.view selectorConfig ctx
+                    KeywordMultiSelector.view selectorConfig ctx
 
                 Edit ->
                     editor model
