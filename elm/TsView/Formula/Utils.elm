b06847200c013d044f16caa5b80800c0cfc4bdcd
André Espaze <dede@pythonian.fr>
2020-05-04 11:33:36 +0200

    Update Utils

--- elm/TsView/Formula/Utils.elm
+++ elm/TsView/Formula/Utils.elm
@@ -1,15 +1,28 @@
 module TsView.Formula.Utils exposing
-    ( boolParser
+    ( Path
+    , boolParser
     , boolToString
     , buildForest
     , buildTree
+    , foldEither
+    , icon
     , numberParser
+    , openAll
+    , selectPath
+    , sendCmd
+    , strPath
     , stringParser
     , valueParser
+    , withPath
     )
 
+import Either exposing (Either(..))
+import Html as H
+import Html.Attributes as A
+import List.Extra exposing (getAt)
 import Parser exposing ((|.), (|=), Parser)
 import Set
+import Task
 import Tree exposing (Tree)
 import Tree.Zipper as Zipper exposing (Zipper)
 import TsView.Formula.Spec.Type as S
@@ -122,3 +135,71 @@ valueParser inputType =
         S.SearchString ->
             stringParser
                 |> andThen (\x -> ( x, S.StringValue x ))
+
+
+openAll : Zipper a -> List (Zipper a)
+openAll parent =
+    let
+        collect zipper children =
+            case Zipper.nextSibling zipper of
+                Nothing ->
+                    List.reverse children
+
+                Just child ->
+                    collect child (child :: children)
+    in
+    case Zipper.firstChild parent of
+        Nothing ->
+            []
+
+        Just firstChild ->
+            collect firstChild [ firstChild ]
+
+
+type alias Path =
+    List Int
+
+
+strPath : Path -> String
+strPath path =
+    "[" ++ String.join ", " (List.map String.fromInt path) ++ "]"
+
+
+selectPath : Path -> Zipper a -> Maybe (Zipper a)
+selectPath path zipper =
+    case path of
+        [] ->
+            Just zipper
+
+        i :: is ->
+            getAt i (openAll zipper) |> Maybe.andThen (selectPath is)
+
+
+withPath : Path -> Tree a -> (Zipper a -> Either String b) -> Either String b
+withPath path tree f =
+    case selectPath path (Zipper.fromTree tree) of
+        Just zipper ->
+            f zipper
+
+        Nothing ->
+            Left <| strPath path ++ " not found"
+
+
+foldEither : (a -> b -> Either x b) -> b -> List a -> Either x b
+foldEither f b la =
+    case la of
+        x :: xs ->
+            f x b |> Either.andThen (\rb -> foldEither f rb xs)
+
+        [] ->
+            Either.singleton b
+
+
+sendCmd : (a -> msg) -> a -> Cmd msg
+sendCmd toMsg a =
+    Task.succeed a |> Task.perform toMsg
+
+
+icon : String -> H.Html msg
+icon cls =
+    H.i [ A.class cls ] []
