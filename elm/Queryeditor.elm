48fd43274a0e7ffefa6a6323439f5b9e01553a91
Aurélien Campéas <aurelien.campeas@pythonian.fr>
2024-03-01 18:49:28 +0100

    qeditor: present basket names in a dropdown and fetch what the user asked
    Lastly, when fetched, we display the basket definition in full.

--- elm/Queryeditor.elm
+++ elm/Queryeditor.elm
@@ -1,9 +1,13 @@
 module Queryeditor exposing ( main )
 
 import Browser
+import Dict
 import Html as H
+import Html.Attributes as HA
+import Html.Events as HE
 import Http
 import Json.Decode as JD
+import Lisp
 import Url.Builder as UB
 import Util as U
 
@@ -14,11 +18,16 @@ type alias Model =
     , errors : List String
     -- baskets
     , baskets : List String
+    -- current basket
+    , name : Maybe String
+    , basket : Maybe String
     }
 
 
-type Msg =
-    GotBasketNames (Result Http.Error String)
+type Msg
+    = GotBasketNames (Result Http.Error String)
+    | SelectedBasket String
+    | GotBasketDefinition (Result Http.Error String)
 
 
 getbaskets model =
@@ -29,15 +38,48 @@ getbaskets model =
         }
 
 
+getbasket model name =
+    Http.get
+        { expect = Http.expectString GotBasketDefinition
+        , url = UB.crossOrigin model.baseurl
+                [ "api",  "series", "basket-definition" ]
+                [ UB.string "name" name ]
+        }
+
+
 view model =
     let
-        showbasket name =
-            H.span [] [ H.text <| name ++ " " ]
+        unpacksbasket basketname =
+            JD.succeed <| SelectedBasket basketname
 
+        basketoption basketname =
+            H.option
+                [ HA.value basketname ]
+                [ H.text basketname ]
+
+        basketslist =
+            [ H.select
+                  [ HA.name "basket"
+                  , HE.on "change" (JD.andThen unpacksbasket HE.targetValue)
+                  , HA.class "form-control"
+                  ]
+                  <| List.map basketoption model.baskets
+            ]
+
+        currentbasket =
+            case model.basket of
+                Just basket ->
+                    case Lisp.parse basket of
+                        Just tree -> H.div [] (Lisp.view tree Dict.empty )
+                        Nothing -> H.span [] []
+
+                Nothing ->
+                    H.span [] []
     in
     H.div []
         [ H.h1 [] [ H.text "Baskets" ]
-        , H.div [] <| List.map showbasket model.baskets
+        , H.div [] basketslist
+        , H.div [] [ currentbasket ]
         ]
 
 
@@ -50,12 +92,33 @@ update msg model =
         GotBasketNames (Ok rawbaskets) ->
             case JD.decodeString (JD.list JD.string) rawbaskets of
                 Ok baskets ->
-                    U.nocmd { model | baskets = baskets }
+                    ( { model | baskets = baskets }
+                    , case List.head baskets of
+                          Nothing -> Cmd.none
+                          Just head -> getbasket model head
+                    )
+
                 Err err ->
                     doerr "getbasketnames decode" <| JD.errorToString err
 
         GotBasketNames (Err err) ->
             doerr "getbasketnames http" <| U.unwraperror err
+
+        SelectedBasket name ->
+            ( { model | name = Just name }
+            , getbasket model name
+            )
+
+        GotBasketDefinition (Ok rawbasket) ->
+            case JD.decodeString JD.string rawbasket of
+                Ok def ->
+                    U.nocmd { model | basket = Just def }
+
+                Err err ->
+                    doerr "getbasketdefinition decode" <| JD.errorToString err
+
+        GotBasketDefinition (Err err) ->
+            doerr "getbasketdefinition http" <| U.unwraperror err
 
 
 type alias Input =
@@ -71,6 +134,8 @@ main =
                        { baseurl = input.baseurl
                        , errors = []
                        , baskets = []
+                       , name = Nothing
+                       , basket = Nothing
                        }
                in
                ( model, getbaskets model )
