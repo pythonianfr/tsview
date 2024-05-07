1c5498857e38a756b5592e1d0e031dd2943efe42
Arnaud Campeas[arnaud.campeas@pythonian.fr] <Arnaud Campeas[arnaud.campeas@pythonian.fr]>
2024-05-07 12:32:51 +0200

    menu/tseditor: add stand alone menu

--- tsview/blueprint.py
+++ tsview/blueprint.py
@@ -513,10 +513,12 @@ def tsview(tsa):
         if not has_roles('admin', 'rw', 'ro'):
             return 'Nothing to see there.'
 
+        flags_menu = json.dumps([homeurl(), 'timeseries-catalog'])
         return render_template(
             'tseditor.html',
             homeurl=homeurl(),
-            name=request.args.get('name')
+            name=request.args.get('name'),
+            flags_menu=flags_menu,
         )
 
     # menu
