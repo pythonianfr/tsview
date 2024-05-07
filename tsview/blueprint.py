44e85f39da23ac8db45cfd69b33f5426f3dde36a
Arnaud Campeas[arnaud.campeas@pythonian.fr] <Arnaud Campeas[arnaud.campeas@pythonian.fr]>
2024-05-07 15:22:31 +0200

    menu/tsinfo: implement stand-alone mneu

--- tsview/blueprint.py
+++ tsview/blueprint.py
@@ -387,11 +387,12 @@ def tsview(tsa):
     def tsinfo():
         if not has_roles('admin', 'rw', 'ro'):
             return 'Nothing to see there.'
-
+        flags_menu = json.dumps([homeurl(), 'timeseries-catalog'])
         return render_template(
             'tsinfo.html',
             homeurl=homeurl(),
-            name=request.args.get('name')
+            name=request.args.get('name'),
+            flags_menu=flags_menu,
         )
 
     @bp.route('/groupinfo')
