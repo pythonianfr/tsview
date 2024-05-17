10401ae975b2f48c013eba554a1cd38412bc4bfe
Auguste Pezin <auguste.pezin@pythonian.fr>
2024-05-17 14:52:48 +0200

    Formulas: create json decoder
    We create the decoder that will read the data retrieved by the request
    and save it into the model.

--- elm/Formulas.elm
+++ elm/Formulas.elm
@@ -3,16 +3,67 @@ import Browser
 import Http
 import Html as H
 import Url.Builder as UB
+import Json.Decode as D
+import Util as U
 
 
 type alias Model =
     { baseurl : String
+    , formulas : List Formula
     , name : String
+    , errors : List String
     }
 
 
 type Msg =
     GotFormulas (Result Http.Error String)
+
+
+type alias Imeta =
+    { formula: String
+    , tzaware : Bool
+    , index_type : String
+    , value_type: String
+    , contenthash : String
+    , index_dtype : String
+    , value_dtype : String
+    }
+
+
+type alias Formula =
+    { name : String
+    , imeta : Imeta
+    , meta : {}
+    , source : String
+    , kind : String
+    }
+
+
+decodeImeta : D.Decoder Imeta
+decodeImeta =
+    D.map7 Imeta
+        (D.field "formula" D.string)
+        (D.field "tzaware" D.bool)
+        (D.field "index_type" D.string)
+        (D.field "value_type" D.string)
+        (D.field "contenthash" D.string)
+        (D.field "index_dtype" D.string)
+        (D.field "value_dtype" D.string)
+
+
+decodeFormula : D.Decoder Formula
+decodeFormula =
+    D.map5 Formula
+        (D.field "name" D.string)
+        (D.field "imeta" decodeImeta)
+        (D.field "meta" (D.succeed {}))
+        (D.field "source" D.string)
+        (D.field "kind" D.string)
+
+
+decodeListFormulas : D.Decoder (List Formula)
+decodeListFormulas =
+    D.list decodeFormula
 
 
 view : Model -> H.Html Msg
@@ -22,11 +73,20 @@ view model =
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
+    let
+        doerr tag error =
+            U.nocmd <| U.adderror model (tag ++ " -> " ++ error)
+    in
     case msg of
         GotFormulas (Ok listFormulas) ->
-            ( model, Cmd.none )
+            case D.decodeString decodeListFormulas listFormulas of
+                Ok formulas ->
+                    U.nocmd { model | formulas = formulas }
+                Err err ->
+                    doerr "getseries decode" <| D.errorToString err
+
         GotFormulas (Err err) ->
-            ( model, Cmd.none )
+            doerr "getseries http" <| U.unwraperror err
 
 
 getSeriesInfo : Model -> Cmd Msg
@@ -53,6 +113,8 @@ main =
             let
                 model =
                     { baseurl = input.baseurl
+                    , errors = []
+                    , formulas = []
                     , name = input.name
                     }
             in ( model , getSeriesInfo model )
