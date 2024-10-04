61e03a7b0829dbe153c8e8950530d32a7831ff6e
Aurélien Campéas <aurelien.campeas@pythonian.fr>
2024-10-04 15:27:33 +0200

    templates: de-jinjaize the plot_figure.js module
    It used to be "templatized". Is no more.
    We want it to become a normal js module again.
    
    closes #31

---
+++ tsview/tsview_static/plot_figure.js
@@ -0,0 +1,85 @@
+class PlotFigure extends HTMLElement {
+    static get observedAttributes() {
+        return ['args', 'history-args', 'hover-args'];
+    }
+
+    attributeChangedCallback(name, old_value, new_value) {
+        if ( name == 'args' ) {
+            function doit() {
+                let args = JSON.parse(new_value);
+                Plotly.newPlot(
+                    args.div,
+                    args.data,
+                     args.layout,
+                    args.config
+                );
+                document.getElementById(args.div).on(
+                    'plotly_relayout', function(eventData) {
+                        let range = ["", ""]
+                        let panactive = false
+                        if ('dragmode' in eventData) {
+                            if (eventData['dragmode'] == 'pan') {
+                                panactive = true;
+                            };
+                            app.ports.panActive.send(panactive);
+                        };
+                        if ('xaxis.autorange' in eventData &&
+                            'yaxis.autorange' in eventData) {
+                            app.ports.dateInInterval.send(range);
+                        };
+                        if ('xaxis.range[0]' in eventData &&
+                            'xaxis.range[1]' in eventData) {
+                            range = [
+                                eventData['xaxis.range[0]'],
+                                eventData['xaxis.range[1]']
+                            ];
+                            app.ports.dateInInterval.send(range);
+                        };
+                    }
+                );
+            }
+            setTimeout(doit, 10)
+        }
+        if ( name == 'history-args' ) {
+            function doit () {
+                let history_args = JSON.parse(new_value);
+                Plotly.newPlot(
+                    history_args.div,
+                    history_args.data,
+                    history_args.layout,
+                );
+                document.getElementById(history_args.div).on('plotly_hover', function(data) {
+                    var points = data.points;
+                    var list_data = [];
+                    points.forEach(function(point){
+                        list_data.push(
+                            {
+                                "date": point.data.name,
+                                "value": point.y
+                            }
+                        );
+                     });
+                    var data = {
+                        "name" : points[0].x,
+                        "data" : list_data
+                    };
+
+                    app.ports.dataFromHover.send(JSON.stringify(data));
+                });
+             }
+             setTimeout(doit, 10)
+         }
+         if ( name == 'hover-args' ) {
+             function doit () {
+                 let hover_args = JSON.parse(new_value);
+                 Plotly.newPlot(
+                     hover_args.div,
+                     hover_args.data,
+                     hover_args.layout,
+                 );
+             }
+             setTimeout(doit, 10)
+         }
+    }
+}
+window.customElements.define("plot-figure", PlotFigure);
