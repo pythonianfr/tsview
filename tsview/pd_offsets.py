bd31bcd75b007bf15bf527c1a25a6b3361f83505
Arnaud Campeas[arnaud.campeas@pythonian.fr] <Arnaud Campeas[arnaud.campeas@pythonian.fr]>
2025-01-10 15:29:37 +0100

    offsets: restrict values

--- tsview/pd_offsets.py
+++ tsview/pd_offsets.py
@@ -11,35 +11,35 @@
 
 OFFSETS = [
     'B',
-    'C',
+    # 'C',
     'W',
-    'WOM',
-    'LWOM',
+    #'WOM', -> "Prefix \'WOM\' requires a suffix."
+    #'LWOM',
     'ME',
     'MS',
-    'BME',
-    'BMS',
-    'CBME',
-    'CBMS',
-    'SME',
-    'SMS',
+    'BME', # Business month end
+    'BMS', # Business month start
+    # 'CBME',
+    # 'CBMS',
+    'SME', # every 15th and last day of the month
+    'SMS', # every first day and 15th of the month
     'QE',
     'QS',
     'BQE',
     'BQS',
-    'REQ',
+    # 'REQ', REQ, failed to parse with error message: TypeError('_parse_suffix() takes exactly 3 positional arguments (0 given)')"
     'YE',
     'YS',
     'BYE',
     'BYS',
-    'RE',
-    'bh',
-    'cbh',
+    #'RE', RE, failed to parse with error message: TypeError('_parse_suffix() takes exactly 3 positional arguments (0 given)')")
+    'bh', # business hours (from 9 to 16 every business day)
+    # 'cbh',
     'D',
     'h',
     'min',
     's',
-    'ms',
-    'us',
-    'ns'
+    # 'ms',
+    # 'us',
+    # 'ns'
 ]
